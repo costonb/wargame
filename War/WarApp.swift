fc8e833568699c06e04af7fef4b4496f5b5426e9
Brandon Coston <brandon.coston@prolifiq.com>
2023-02-10 16:04:42 -0800

    Initial Commit

---
+++ War/WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarApp.swift
+//  War
+//
+//  Created by Brandon Coston on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
